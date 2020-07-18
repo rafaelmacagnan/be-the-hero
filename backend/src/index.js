803ef20ec05de360a2cf8adc79d531885964d90f
rafaelmacagnan <rafaelmacagnansantos@gmail.com>
2020-07-17 22:52:31 -0300

    origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
-const cors = require('cors')
+const validacors = require('cors')
 
 const app = express();
 
-app.use(cors());
+app.use(validacors());
 
 app.use(express.json());
 app.use(routes);
